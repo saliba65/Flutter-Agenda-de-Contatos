ce5d57f5c29c90547298878f131f607154906b79
Lucas Saliba <lucassaliba10@gmail.com>
2020-11-25 15:51:53 -0300

    teste

--- Agenda++/agenda/agenda/android/app/src/main/kotlin/com/example/agenda/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.agenda
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Agenda++/agenda/agenda/lib/routes/app_routes.dart
+++
@@ -1,5 +0,0 @@
-class AppRoutes {
-  static const HOME = '/';
-  static const USER_FORM = '/user-form';
-  static const USER_CALENDAR = '/user-calendar';
-}
